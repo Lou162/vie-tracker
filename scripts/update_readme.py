9e66c7ab5694bbca094e61955233ec84b2cfe365
Lou162 <louna.libbrechtgmail.com>
2026-06-20 14:51:31 +0200

    refacto to have small code

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -9,26 +9,18 @@ def update_readme():
         return
     readme_file = open("./README.md","r+",encoding = "utf-8")
     readme_lines = readme_file.readlines()
-    dateLine = readme_lines.index("<!-- Date de mise à jour -->\n")
-    readme_lines[dateLine+2] = f"**Dernière mise à jour:** {datetime.now().strftime('%d/%m/%Y')}\n"
+    updateDate(readme_lines)
     for country, offers in result.items():
         print(f"Updating offers for {country}...")
-        title_line = readme_lines.index(f"<!-- Title {country} -->\n")
-        readme_lines[title_line+2] = f"## {country} <span style='color:gray'>({len(offers)} offres)</span>\n"
+        updateCountOffers(country, offers, readme_lines)
         line = readme_lines.index(f"<!-- Ici les offres pour le {country} -->\n")
         end_line = readme_lines.index(f"<!-- Fin des offres pour le {country} -->\n")
         offers_details=[]
         for offer in offers:
-            if(offer['link'] == ""):
-                offer['link'] = "N/A"
-            else:
-                offer['link'] = f"[Lien]({offer['link']})"
-
             if(country == "🏆most Recent 🏆"):
                 offers_details.append(f"{offer['company']} | {offer['mission']} | {offer['country']} | {offer['duration']} | {offer['contact']} | {offer['link']}\n")
             else:    
                 offers_details.append(f"{offer['company']} | {offer['mission']} | {offer['duration']} | {offer['contact']} | {offer['link']}\n")
-                print(f"Adding offer: {offer['company']} | {offer['mission']} | {offer['duration']} | {offer['contact']} | {offer['link']}")
         readme_lines[line+4:end_line] = offers_details
     readme_file.close()
     readme_file = open("./README.md","w+",encoding = "utf-8")
@@ -37,5 +29,15 @@ def update_readme():
     #close both the files.
     readme_file.close()
     print("README.md updated successfully.")
+
+def updateDate(readme_lines):
+    dateLine = readme_lines.index("<!-- Date de mise à jour -->\n")
+    readme_lines[dateLine+2] = f"**Dernière mise à jour:** {datetime.now().strftime('%d/%m/%Y')}\n"
+
+
+def updateCountOffers(country, offers, readme_lines):
+    title_line = readme_lines.index(f"<!-- Title {country} -->\n")
+    readme_lines[title_line+2] = f"## {country} <span style='color:gray'>({len(offers)} offres)</span>\n"
+
 update_readme()
 
